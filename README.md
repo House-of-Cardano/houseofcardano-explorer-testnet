f814b1c8a2069b809da6a8598eeb3a8b153a1fb1
simonholmes001 <simonholmesabc@gmail.com>
2022-03-10 18:06:48 +0100

    Start writing unit tests to test API, fixes AB#348

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# query-blockchain-testnet
+# houseofcardano-explorer-testnet
 Backend server to query the Cardano blockchain (testnet)
+
+## Set-up for TDD
+
+Import `supertest` and `jest` testing environment:
+
+``` markdown
+npm install supertest jest @types/jest @types/supertest --save-dev
+```
+
+Setup a basic configuration file by running `jest --init` (perhaps need to run `npm install jest --global` first?).
+
--- routes/query.js
+++ routes/query.js
@@ -12,9 +12,12 @@ router.get('/query-db', getInformation.getQuery);
 
 //   text: 'select utxo_view.tx_id, utxo_view.address, utxo_view.value, tx.hash, tx.block_id from utxo_view inner join tx on tx.id = utxo_view.tx_id inner join tx_out on tx.id = tx_out.tx_id where utxo_view.address = $1 and tx_out.data_hash = $2',
 
+const addr = 'addr_test1wzhfye4zxffxd59gg0fhjzavy7uuhpul04kr5myavevh29svlsrpc';
+const datumHash =  "\\xfac96da1bf190d85ae7e7a45b07b95826c3eb91b839564959d8411d4e0dc089c";
+
 const querydb = {
   text: 'select utxo_view.tx_id, utxo_view.address, utxo_view.value, tx.hash::text, tx_out.data_hash::text, tx.block_id from utxo_view inner join tx on tx.id = utxo_view.tx_id inner join tx_out on tx.id = tx_out.tx_id where utxo_view.address = $1 and tx_out.data_hash = $2',
-  values: ['addr_test1wzhfye4zxffxd59gg0fhjzavy7uuhpul04kr5myavevh29svlsrpc', "\\xfac96da1bf190d85ae7e7a45b07b95826c3eb91b839564959d8411d4e0dc089c"],
+  values: [addr, datumHash],
   rowMode: 'array'
 }
 
