01ecf7693ff7581457f80d8183b33339bb637182
simonholmes001 <simonholmesabc@gmail.com>
2022-03-11 13:54:15 +0100

    Update README

--- routes/query.js
+++ routes/query.js
@@ -5,13 +5,6 @@ const db = require('../util/cardano-db');
 
 const router = express.Router();
 
-router.get('/query-db', getInformation.getQuery);
-
-// utxo_view.tx_id, utxo_view.address, utxo_view.value, tx.hash, tx_out.data_hash, tx.block_id 
-// encode(cast(tx_out.data_hash) as bytea), 
-
-//   text: 'select utxo_view.tx_id, utxo_view.address, utxo_view.value, tx.hash, tx.block_id from utxo_view inner join tx on tx.id = utxo_view.tx_id inner join tx_out on tx.id = tx_out.tx_id where utxo_view.address = $1 and tx_out.data_hash = $2',
-
 const addr = 'addr_test1wzhfye4zxffxd59gg0fhjzavy7uuhpul04kr5myavevh29svlsrpc';
 const datumHash =  "\\xfac96da1bf190d85ae7e7a45b07b95826c3eb91b839564959d8411d4e0dc089c";
 
